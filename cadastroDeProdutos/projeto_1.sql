aab42cbf48205456a10193112d0cf8781b350046
Gustavo Messias Santos <gustavopessoal2023@gmail.com>
2023-09-27 17:36:09 -0300

    Aula 27/09/2023

---
+++ cadastroDeProdutos/projeto_1.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE cadastro_de_produtos_209068;
+
+USE cadastro_de_produtos_209068;
+
+select * from produto;
+
+desc produto
--- cadastroDeProdutos/src/main/java/br/com/rafaelson/controller/ProdutoController.java
+++ cadastroDeProdutos/src/main/java/br/com/rafaelson/controller/ProdutoController.java
@@ -1,45 +1,65 @@
 package br.com.rafaelson.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rafaelson.model.Produto;
 import br.com.rafaelson.repository.ProdutoRepository;
 
-@Controller
-@ResponseBody
+@RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
-	/*@GetMapping("/olamundo")
-	public String olaMundo() {
-		return "Olá Mundo";
-	}*/
-	
+	/*
+	 * @GetMapping("/olamundo") public String olaMundo() { return "Olá Mundo"; }
+	 */
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
-	@GetMapping("/listarprodutos")
+
+	@GetMapping
 	public List<Produto> listarProdutos() {
-		
-		/*Produto p1 = new Produto("Celular Samsung", 35, 1299.99);
-		Produto p2 = new Produto("Cafeteira Arno", 10, 199.99);
-		Produto p3 = new Produto("Mouse sem Fio", 150, 9.95);
-		
-		return Arrays.asList(p1, p2, p3);*/
-		
+
+		/*
+		 * Produto p1 = new Produto("Celular Samsung", 35, 1299.99); Produto p2 = new
+		 * Produto("Cafeteira Arno", 10, 199.99); Produto p3 = new
+		 * Produto("Mouse sem Fio", 150, 9.95);
+		 * 
+		 * return Arrays.asList(p1, p2, p3);
+		 */
+
 		List<Produto> produtos = produtoRepository.findAll();
 		return produtos;
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Produto> findById(@PathVariable Long id) {
 		return produtoRepository.findById(id).map(objetoGravado -> ResponseEntity.ok().body(objetoGravado))
-		.orElse(ResponseEntity.notFound().build());
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	public ResponseEntity<Produto> cadastrarProduto(@RequestBody Produto produto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto));
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Produto> atualizarProduto(@PathVariable Long id, @RequestBody Produto produto) {
+		return produtoRepository.findById(id).map(objetoGravado -> {
+			objetoGravado.setNomeProduto(produto.getNomeProduto());
+			objetoGravado.setQuantidade(produto.getQuantidade());
+			objetoGravado.setPreco(produto.getPreco());
+			Produto produtoAtualizado = produtoRepository.save(objetoGravado);
+			return ResponseEntity.ok().body(produtoAtualizado);
+		}).orElse(ResponseEntity.notFound().build());
 	}
 }
